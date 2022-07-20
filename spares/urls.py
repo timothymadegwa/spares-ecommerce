cf479268e41ff6bd71883fa6e9156b5706801862
Timothy Madegwa <timothymadegwa@gmail.com>
2022-07-20 20:23:52 +0200

    made all initial pages functional in django

--- spares/urls.py
+++ spares/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
+    path('login', views.login, name = 'login'),
+    path('cart', views.cart, name = 'cart'),
+    path('checkout', views.checkout, name = 'checkout'),
+    path('spares', views.spares, name = 'spares'),
+
 ]
--- spares/views.py
+++ spares/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def login(request):
+    return render(request, 'spares/login.html')
+
 # @login_required(login_url='login')
 def home(request):
     return render(request, 'spares/home.html')
+
+def cart(request):
+    return render(request, 'spares/cart.html')
+
+def checkout(request):
+    return render(request, 'spares/checkout.html')
+
+def spares(request):
+    return render(request, 'spares/spares.html')
+
