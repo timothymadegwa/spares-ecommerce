32e24afcfcf9bfe10de17004500f8b9ab63636a6
Timothy Madegwa <timothymadegwa@gmail.com>
2022-07-23 19:06:33 +0200

    added the inventory model

--- spares/admin.py
+++ spares/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Inventory
+#from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'category', 'brand', 'quantity','price', 'discount', 'is_displayed')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_displayed',)
+    list_filter = ('category', 'brand')
+    search_fields = ('name',)
+
+admin.site.register(Inventory, InventoryAdmin)
+
+
+admin.site.site_header = "Spares Shop KE"
--- spares/models.py
+++ spares/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+CHOICES = (
+    ('Perfume', 'Perfume'),
+    ('Cosmetics', 'Cosmetics'),
+)
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    brand = models.CharField(max_length=100, choices=CHOICES)
+    category = models.CharField(max_length=100, choices=CHOICES)
+    quantity = models.PositiveIntegerField(default=0, blank=False, null=False)
+    price = models.FloatField(null=False, blank=False)
+    is_displayed = models.BooleanField(default=True)
+    has_discount = models.BooleanField(default=False)
+    discount = models.FloatField(blank=False, null=False)
+    photo = models.ImageField(upload_to='photos/spares')
+           
+    class Meta:
+            
+        verbose_name = 'inventory'
+        verbose_name_plural = 'inventories'
+
+
+    def total(self):
+        return round(self.quantity * self.price, 2)
+'''
+    @property
+    def quantity_sold(self):
+        saleitems = self.saleitem_set.all()
+        total = 0
+        for item in saleitems:
+            total += item.quantity_sold
+'''
