1d4a3dadbef1920079bf0be0104c8c822ba84799
Timothy Madegwa <timothymadegwa@gmail.com>
2022-09-08 11:49:34 +0300

    added category and brand to admin page

--- spares/admin.py
+++ spares/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
-from .models import Inventory
+from .models import Inventory, Category, Brand
 #from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('id','name', 'category', 'quantity','price', 'discount', 'is_displayed')
+    list_display = ('id','name', 'category','brand', 'quantity','price', 'discount', 'is_displayed')
     list_display_links = ('id', 'name')
     list_editable = ('is_displayed',)
-    list_filter = ('category',)
+    list_filter = ('category', 'brand')
     search_fields = ('name',)
 
 admin.site.register(Inventory, InventoryAdmin)
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brand_name')
+
+admin.site.register(Brand, BrandAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name')
+
+admin.site.register(Category, CategoryAdmin)
 
 admin.site.site_header = "Spares Shop KE"
--- spares/models.py
+++ spares/models.py
@@ -5,7 +5,7 @@ class Brand(models.Model):
     brand_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.brand_name
 
 class Category(models.Model):
     category_name = models.CharField(max_length=100, unique=True)
@@ -14,23 +14,23 @@ class Category(models.Model):
         verbose_name_plural = "categories"
     
     def __str__(self):
-        return self.name
+        return self.category_name
 
 class Inventory(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, default=1)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0, blank=False, null=False)
     price = models.FloatField(null=False, blank=False)
     is_displayed = models.BooleanField(default=True)
     has_discount = models.BooleanField(default=False)
-    discount = models.FloatField(blank=False, null=False)
+    discount = models.FloatField(blank=False, null=False, default=0)
     photo = models.ImageField(upload_to='photos/spares')
            
     class Meta:
             
         verbose_name = 'inventory'
-        verbose_name_plural = 'inventories'
+        verbose_name_plural = 'inventory'
     
     def __str__(self):
         return self.name
