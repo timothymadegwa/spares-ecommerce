4b963f1c065f328b9cef148b3da3dfb6e7e88282
Timothy Madegwa <timothymadegwa@gmail.com>
2022-09-10 21:58:45 +0300

    calculated totals in cart

--- spares/static/js/cart.js
+++ spares/static/js/cart.js
@@ -1,5 +1,5 @@
 /*const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;*/
-var updateBtns = document.getElementsByClassName('update-cart')
+let updateBtns = document.getElementsByClassName('update-cart')
 
 for (i = 0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
